093bc0e11a20189e86ee48e7d9751c5690581417
ArchitKC <archithrd@gmail.com>
2020-08-17 13:18:30 -0400

    Add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { AccountComponent } from './account/account.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+import { RecipeInitialComponent } from './recipe/recipe-initial/recipe-initial.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoute: Routes = [
+  {path: '', redirectTo: 'recipe',  pathMatch: 'full'},
+  {path: 'recipe', component: RecipeComponent,
+  children: [
+    {path: '', component: RecipeInitialComponent},
+    {path: 'edit', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent}
+  ]},
+  {path: 'shoppingList', component: ShoppingListComponent},
+  {path: 'viewService', component: AccountComponent},
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: 'not-found'}
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoute)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
 import { RecipeComponent } from './recipe/recipe.component';
@@ -12,29 +13,15 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RecipeInitialComponent } from './recipe/recipe-initial/recipe-initial.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+
 import { DropdownDirective } from './shared/dropdown.directive';
 
 import { LoggingServices } from './services/logging.services';
 import { AccountServices } from './services/account.services';
 import { ShoppingListService } from './services/shoppinglist.services';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RecipeInitialComponent } from './recipe/recipe-initial/recipe-initial.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
-
-const appRoute: Routes = [
-  {path: '', redirectTo: 'recipe',  pathMatch: 'full'},
-  {path: 'recipe', component: RecipeComponent,
-  children: [
-    {path: '', component: RecipeInitialComponent},
-    {path: 'edit', component: RecipeEditComponent},
-    {path: ':id', component: RecipeDetailComponent},
-    {path: ':id/edit', component: RecipeEditComponent}
-  ]},
-  {path: 'shoppingList', component: ShoppingListComponent},
-  {path: 'viewService', component: AccountComponent},
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: 'not-found'}
-];
 
 @NgModule({
   declarations: [
@@ -56,7 +43,7 @@ const appRoute: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoute)
+    AppRoutingModule
   ],
   providers: [
     AccountServices,
